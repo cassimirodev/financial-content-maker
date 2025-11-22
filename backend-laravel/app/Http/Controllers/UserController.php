242ebe8d4ed845103e2fd069507ad18b045a02a2
Luís Eduardo <luiseduardocass06@gmail.com>
2025-11-22 03:18:02 -0300

    feat: add user controller to manage user register

---
+++ backend-laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new user.
+     */
+    public function store(RegisterRequest $request): JsonResponse
+    {
+
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return response()->json([
+            'message' => 'Usuário corporativo cadastrado com sucesso.',
+            'user' => $user
+        ], 201);
+    }
+}
