f85599c7916bb41dd8b5ae5fa3dde9bc19d11731
Luís Eduardo <luiseduardocass06@gmail.com>
2025-11-22 04:59:21 -0300

    fix: fix sanctrum missing package

--- backend-laravel/app/Http/Requests/ConteudoRequest.php
+++ backend-laravel/app/Http/Requests/ConteudoRequest.php
@@ -12,7 +12,7 @@ class ConteudoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return $this->authorize();
     }
 
     /**
--- backend-laravel/app/Http/Requests/GeracaoRequest.php
+++ backend-laravel/app/Http/Requests/GeracaoRequest.php
@@ -12,7 +12,7 @@ class GeracaoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return $this->authorize();
     }
 
     /**
